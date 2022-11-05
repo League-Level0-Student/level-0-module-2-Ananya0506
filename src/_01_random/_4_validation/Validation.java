2cf18ef0b0d303599ac7af0c366a7888a1f187cc
smellstink <54760319+OceanNut420@users.noreply.github.com>
2022-11-04 18:24:36 -0700

    i started module 2 and had fun with the magic 8 ball thingy.

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -17,9 +17,129 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
 
 		// 2. Repeat all the code above 10 times
-		
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
+		if (randomNumber == 0) {
+			JOptionPane.showMessageDialog(null, "wow! you are good at everything you do!");
+		} else if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null, "you are smart and knowledgable! your intelligence will take you far!");
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null, "you are so kind and caring! you will make people happy everywhere you go!");
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null, "!your amazing physical abilities will definetly make you popular!");
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null, "your leadership abilities will make you known all over the world!");
+		}
 		// 3. Find someone to test out your program. They will like it :)
 	}
 }
--- src/_01_random/_5_magic_8_ball/Magic8Ball.java
+++ src/_01_random/_5_magic_8_ball/Magic8Ball.java
@@ -6,32 +6,44 @@ package _01_random._5_magic_8_ball;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
 	// 1. Make a main method that includes all the steps below
-
-	// 2. Get the user to enter a question for the 8 ball to answer
+	public static void main(String[] args) {
+		
 	
+	// 2. Get the user to enter a question for the 8 ball to answer
+	JOptionPane.showInputDialog("hello! this is a place where you can find an answer to all your questions! type any question you'd like, and the Magic 8 ball will answer it for you!");
 
 	// 3. Make a variable and initialize it to a random number.
 	//     ** You will need to make a random object!
 	//     Limit the random numbers to be between 0 and 3
-
-	
+Random ran= new Random();
+int randomNumber = ran.nextInt(3);
 	// 4. If the random number is 0
-
+if (randomNumber == 0) {
+	JOptionPane.showMessageDialog(null,"Yes");
+}
 	// -- tell the user "Yes"
 
 	// 5. If the random number is 1
-
+if (randomNumber == 1) {
+	JOptionPane.showMessageDialog(null,"No");
+}
 	// -- tell the user "No"
 
 	// 6. If the random number is 2
-
+if (randomNumber == 2) {
+	JOptionPane.showMessageDialog(null,"Maybe you should ask Google?");
+}
 	// -- tell the user "Maybe you should ask Google?"
 
 	// 7. If the random number is 3
-
+if (randomNumber == 3) {
+	JOptionPane.showMessageDialog(null,"ask for help!");
+}
 	// -- write your own answer
-
+	}
 }
