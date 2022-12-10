b6dc474562f2292d684a9517b51ff344bffa1b9d
smellstink <54760319+OceanNut420@users.noreply.github.com>
2022-12-09 18:22:41 -0800

    guh9uh3rgv0j3rg

--- src/_99_extra/NasaCountdown.java
+++ src/_99_extra/NasaCountdown.java
@@ -1,6 +1,12 @@
 
 package _99_extra;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import game_tools.Sound;
+
 /* NASA wants to shoot a rocket into orbit.
  * Somebody has to do the countdown or the rocket won't launch.
  * 
@@ -13,12 +19,22 @@ package _99_extra;
 public class NasaCountdown {
     public static void main(String[] args) throws InterruptedException {
         // 2. Use a pop-up to ask the user where to start counting from
-        
+   String num = JOptionPane.showInputDialog(null,"where do you want to start counting from to launch the NASA rocket?"); 
+   int nummy = Integer.parseInt(num);
         // 3. Change the countdown to use the new starting point
-        
+        int countdown= nummy;
         // 1. Print a countdown from 10 to 0 on the console
-            
-            // 4. Use the Sound.speak method to hear the countdown.
+           while(countdown > 0) {
+        	   System.out.println(countdown);
+        	   Thread.sleep(1000);
+        	   countdown= countdown-1;
+        	  }
+            if (countdown==0) {
+            	Sound.speak("blastoff!");
+            } 
+        		 
+        	
+            // 4. Use the  method to hear the countdown.
             
             // 6. Use the following code to make the program wait one second for each number: Thread.sleep(1000);
             
--- src/_99_extra/SoundtrackToLife.java
+++ src/_99_extra/SoundtrackToLife.java
@@ -9,15 +9,24 @@ public class SoundtrackToLife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+		int userMood = JOptionPane.showOptionDialog(null, "how are you feeling today?", "mood music", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "happy", "sad", "angry", "stressed" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
+     System.out.println(userMood);
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
-
-		
+		if (userMood==3) {
+			playVideo("https://www.youtube.com/watch?v=1ZYbU82GVz4");
+		}
+		if (userMood==2) {
+			playVideo("http://bit.ly/video-for-angry");
+		}
+		if (userMood==1) {
+			playVideo("http://bit.ly/video-for-sad");
+		}
+		if (userMood==0) {
+			playVideo("http://bit.ly/video-for-happy");
+		}
 		// 4. Play different songs for other moods.
 
 /**
